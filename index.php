d8975abaaac8e1d9310e0d8eba78cdd49aea3b4f
ankwachu <54939224+ankwachu@users.noreply.github.com>
2019-11-09 15:35:58 +0100

    Create index.php
    add index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+require 'inc/head.php';
+require 'inc/data/products.php';
+
+
+if(!empty($_GET['add_to_cart'])) {
+    if(empty($_SESSION['cart'][$_GET['add_to_cart']])){
+        $id = $_GET['add_to_cart'];
+        $_SESSION['cart'][$id] = 1;
+    } else {
+        $id = $_GET['add_to_cart'];
+        $quantity = $_SESSION['cart'][$id];
+        $_SESSION['cart'][$id] = $quantity + 1;
+    }
+}
+
+?>
+
+<section class="cookies container-fluid">
+    <div class="row">
+        <?php foreach ($catalog as $id => $cookie) { ?>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                <figure class="thumbnail text-center">
+                    <img src="assets/img/product-<?= $id; ?>.jpg" alt="<?= $cookie['name']; ?>" class="img-responsive">
+                    <figcaption class="caption">
+                        <h3><?= $cookie['name']; ?></h3>
+                        <p><?= $cookie['description']; ?></p>
+                        <a href="?add_to_cart=<?= $id; ?>" class="btn btn-primary">
+                            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
+                        </a>
+                    </figcaption>
+                </figure>
+            </div>
+        <?php } ?>
+    </div>
+</section>
+<?php require 'inc/foot.php'; ?>
